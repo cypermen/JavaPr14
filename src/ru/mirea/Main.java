b58d7fe5b325b22d6332880832a0890d5b70b7cc
Andrey <andreybabich2012@yandex.ru>
2020-12-15 19:28:11 +0300

    new3

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -1,5 +1,7 @@
 package ru.mirea;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,20 +16,19 @@ public class Main {
         //Тест задания 3
         String shirts[] = new String[11];
         shirts[0] = "S001,Black Polo Shirt,Black,XL"; shirts[1] = "S002,Black Polo Shirt,Black,L"; shirts[2] = "S003,Blue Polo Shirt,Blue,XL"; shirts[3] = "S004,Blue Polo Shirt,Blue,M"; shirts[4] = "S005,Tan Polo Shirt,Tan,XL"; shirts[5] = "S006,Black T-Shirt,Black,XL"; shirts[6] = "S007,White T-Shirt,White,XL"; shirts[7] = "S008,White T-Shirt,White,L"; shirts[8] = "S009,Green T-Shirt,Green,S"; shirts[9] = "S010,Orange T-Shirt,Orange,S"; shirts[10] = "S011,Maroon Polo Shirt,Maroon,S";
-        Shirt shirt = new Shirt();
+        ArrayList<Shirt> sh = new ArrayList<>();
         for (String s: shirts) {
-            shirt.addShirt(s);
+            sh.add(new Shirt(s));
         }
-        shirt.output();
+        for(Shirt s: sh) {
+            s.output();
+        }
         //Тест задания 4
         PhoneNumber pn = new PhoneNumber("+79055142541");
         PhoneNumber pn2 = new PhoneNumber("89055142541");
 
         System.out.println(pn);
         System.out.println(pn2);
-        //Kod =
-        //name =
-        //color =
-        //size
+
     }
 }
--- src/ru/mirea/Shirt.java
+++ src/ru/mirea/Shirt.java
@@ -3,22 +3,22 @@ package ru.mirea;
 import java.util.ArrayList;
 
 public class Shirt {
-    private ArrayList<String> shirts = new ArrayList<>();
+    //private ArrayList<String> shirts = new ArrayList<>();
+    private String kod, name, color, size;
 
-    public void addShirt(String s){
-        shirts.add(s);
+    Shirt(String s) {
+        String str[] =  s.split(",");;
+        kod = str[0];
+        name  = str[1];
+        color = str[2];
+        size = str[3];
     }
 
     public void output(){
-        String str[];
-        for (String s: shirts) {
-            str = s.split(",");
-            System.out.println("Kod: " + str[0]);
-            System.out.println("Name: " + str[1]);
-            System.out.println("Color: " + str[2]);
-            System.out.println("Size: " + str[3] + "\n\n");
-            
-        }
+            System.out.println("Kod: " + kod);
+            System.out.println("Name: " + name);
+            System.out.println("Color: " + color);
+            System.out.println("Size: " + size + "\n\n");
     }
 
 }
